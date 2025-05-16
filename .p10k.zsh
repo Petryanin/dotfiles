b76a47038c31c0bfa16c2f4aa92434e8d544c289
as.petryanin <aspetryanin@gmail.com>
2025-05-16 19:52:28 +0300

    .p10k.zsh: fix text color

--- .p10k.zsh
+++ .p10k.zsh
@@ -920,7 +920,7 @@
 
   ##################################[ context: user@hostname ]##################################
   # Context color when running with privileges.
-  typeset -g POWERLEVEL9K_CONTEXT_ROOT_FOREGROUND=1
+  typeset -g POWERLEVEL9K_CONTEXT_ROOT_FOREGROUND=0
   typeset -g POWERLEVEL9K_CONTEXT_ROOT_BACKGROUND=9
   # Context color in SSH without privileges.
   typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_FOREGROUND=3
